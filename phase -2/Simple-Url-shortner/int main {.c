dcc7aa3e030746e78ac8473c22d6de0108bf1214
510Raji <144369824+510Raji@users.noreply.github.com>
2023-10-26 13:26:28 +0530

    Add files via upload

---
+++ phase -2/Simple-Url-shortner/int main {.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <conio.h>
+int main  { 
+    char*ptr; 
+    char string[]= " learn C from dtelms.ap.gov.in " ;
+    ptr=string; 
+    ptr+=6; 
+    printf("%s" ptr);
+    return 0;
+    getch();
+}
